61b62fd4f653de30fbfd8c2fed8d89c69dd18c9a
Tobias Grafke <tg@tp1.rub.de>
2012-11-25 23:43:03 +0100

    gtk2 loading

--- main.cc
+++ main.cc
@@ -1,8 +1,13 @@
 #include "options.h"
 #include "console.h"
 
+#include <gtk/gtk.h>
+
 int main(int argc, char** argv)
 {
   Options options(argc, argv);
-  console::out() << "Hello, world!" << std::endl;
+
+    gtk_init (&argc, &argv);
+    g_set_application_name("truepdf");
+    gtk_main();    
 }

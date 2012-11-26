8f6e81843015d005e5957e818be4315c8fc08620
Tobias Grafke <tg@tp1.rub.de>
2012-11-26 17:12:43 +0100

    simplified and shortened

--- main.cc
+++ main.cc
@@ -48,42 +48,24 @@ int main(int argc, char** argv)
   gtk_init (&argc, &argv);
 
   // PDF STUFF
-  GError *error = NULL;
-
-  gchar *absoluteFileName = getAbsoluteFileName(filename.c_str());
-  gchar *filename_uri = g_filename_to_uri(absoluteFileName, NULL, &error);
-
-  PopplerDocument *pdfDocument = poppler_document_new_from_file(filename_uri, NULL, &error);
-
+  char *absoluteFileName = getAbsoluteFileName(filename.c_str());
+  char *filename_uri = g_filename_to_uri(absoluteFileName, NULL, NULL);
   g_free(absoluteFileName);
+  std::string file(filename_uri);
   g_free(filename_uri);
 
-  if ( NULL == pdfDocument ) {
-    console::out() << "Error loading pdf document!" << std::endl;
-  } else {
-    console::out() << "Pdf document successfully loaded!" << std::endl;
-  }
-
+  PopplerDocument *pdfDocument = poppler_document_new_from_file(file.c_str(), NULL, NULL);
   PopplerPage* page = poppler_document_get_page(pdfDocument, pageNumber-1);
-  if ( page == NULL ) {
-    console::out() << "Error loading page!" << std::endl;
-  } else {
-    console::out() << "Page successfully loaded!" << std::endl;
-  }
 
-  gdouble pageWidth, pageHeight;
+  double pageWidth, pageHeight;
   poppler_page_get_size(page, &pageWidth, &pageHeight);
-  console::out() << "Page is " << pageWidth << "x" << pageHeight << std::endl;
-
 
   SDL_Init(SDL_INIT_VIDEO);
   SDL_Surface* screen = SDL_SetVideoMode(pageWidth*zoom, pageHeight*zoom, 32, SDL_SWSURFACE );
 
-  int stride = screen->pitch;
-  console::out() << "Stride set to " << stride << std::endl;
   unsigned char* data = (unsigned char*) screen->pixels;
-  console::out() << "Buffer for page allocated to " << stride*((int)(pageHeight*zoom)) << " bytes." << std::endl;
-  cairo_surface_t *surface = cairo_image_surface_create_for_data(data, CAIRO_FORMAT_RGB24, pageWidth*zoom, pageHeight*zoom, stride);
+  cairo_surface_t *surface = cairo_image_surface_create_for_data(data, CAIRO_FORMAT_RGB24, pageWidth*zoom, pageHeight*zoom, screen->pitch);
+
   cairo_t* context = cairo_create(surface);
   cairo_save(context);
   cairo_set_source_rgb(context, 1.0, 1.0, 1.0);
@@ -91,10 +73,8 @@ int main(int argc, char** argv)
   cairo_fill(context);
   cairo_restore(context);
   cairo_save(context);
-
   cairo_scale(context, zoom, zoom);
   poppler_page_render(page, context);
-
   cairo_destroy(context);
   cairo_surface_destroy(surface);
 
